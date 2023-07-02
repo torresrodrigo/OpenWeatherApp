a6a8627c57227b768dc9af0db5dbc8c1777b1bb7
torresrodrigo <33295900+torresrodrigo@users.noreply.github.com>
2023-07-02 03:08:04 -0300

    Add Location use cases (#1)

---
+++ OpenWeatherAppCore/OpenWeatherAppCore/Core/Location/Data/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  OpenWeatherAppCore
+//
+//  Created by Rodrigo Torres on 02/07/2023.
+//
+
+import CoreLocation
+
+public struct Location {
+    let latitude: Double
+    let longitude: Double
+}
---
+++ OpenWeatherAppCore/OpenWeatherAppCore/OpenWeatherAppCore.h
@@ -0,0 +1,18 @@
+//
+//  OpenWeatherAppCore.h
+//  OpenWeatherAppCore
+//
+//  Created by Rodrigo Torres on 02/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenWeatherAppCore.
+FOUNDATION_EXPORT double OpenWeatherAppCoreVersionNumber;
+
+//! Project version string for OpenWeatherAppCore.
+FOUNDATION_EXPORT const unsigned char OpenWeatherAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherAppCore/PublicHeader.h>
+
+
